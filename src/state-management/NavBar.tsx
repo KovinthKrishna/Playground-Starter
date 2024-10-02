ca909d92d48eade81f3d4381642200a6c6089982
KovinthKrishna <KovinthKrishna@gmail.com>
2024-10-02 19:35:16 +0530

    Global State Management

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,17 @@
-import LoginStatus from './LoginStatus';
+import LoginStatus from "./auth/LoginStatus";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  return (
-    <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">4</span>
-      <LoginStatus />
-    </nav>
-  );
+    const counter = useCounterStore((s) => s.counter);
+
+    console.log("Render NavBar");
+
+    return (
+        <nav className="navbar d-flex justify-content-between">
+            <span className="badge text-bg-secondary">{counter}</span>
+            <LoginStatus />
+        </nav>
+    );
 };
 
 export default NavBar;
