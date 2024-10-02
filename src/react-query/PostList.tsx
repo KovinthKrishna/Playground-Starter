a9555cda455d33990dced9262153ccb16805ab18
KovinthKrishna <KovinthKrishna@gmail.com>
2024-10-02 10:56:20 +0530

    Fetching and Updating Data with React Query

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,35 +1,42 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import React from "react";
+import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+    const pageSize = 10;
+    const {
+        data: posts,
+        error,
+        isLoading,
+        fetchNextPage,
+        isFetchingNextPage,
+    } = usePosts({ pageSize });
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
+    if (isLoading) return <p>Loading...</p>;
 
-  if (error) return <p>{error}</p>;
+    if (error) return <p>{error.message}</p>;
 
-  return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <>
+            <ul className="list-group">
+                {posts.pages.map((page, index) => (
+                    <React.Fragment key={index}>
+                        {page.map((post) => (
+                            <li key={post.id} className="list-group-item">
+                                {post.title}
+                            </li>
+                        ))}
+                    </React.Fragment>
+                ))}
+            </ul>
+            <button
+                className="btn btn-primary"
+                onClick={() => fetchNextPage()}
+                disabled={isFetchingNextPage}
+            >
+                {isFetchingNextPage ? "Loading..." : "Load More"}
+            </button>
+        </>
+    );
 };
 
 export default PostList;
