8cf61a642dbc087202641cccb22762c068c31bc1
KovinthKrishna <KovinthKrishna@gmail.com>
2024-10-03 13:51:30 +0530

    Routing with React Router

---
+++ src/routing/routes.tsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from "react-router-dom";
+import ContactPage from "./ContactPage";
+import ErrorPage from "./ErrorPage";
+import HomePage from "./HomePage";
+import Layout from "./Layout";
+import LoginPage from "./LoginPage";
+import PrivateRoutes from "./PrivateRoutes";
+import UserDetail from "./UserDetail";
+import UsersPage from "./UsersPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: "contact",
+                element: <ContactPage />,
+            },
+            {
+                path: "login",
+                element: <LoginPage />,
+            },
+        ],
+    },
+    {
+        element: <PrivateRoutes />,
+        children: [
+            {
+                path: "users",
+                element: <UsersPage />,
+                children: [
+                    {
+                        path: ":id",
+                        element: <UserDetail />,
+                    },
+                ],
+            },
+        ],
+    },
+]);
+
+export default router;
